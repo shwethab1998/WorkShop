4b5dfee0a1324c2b658cef57332a840d1b999fd6
“Shwetha <shwethabalaji87@gmail.com>
2020-11-27 10:05:56 +0530

    Start Position is Zero

---
+++ SnakesLadder.java
@@ -0,0 +1,16 @@
+public class SnakesLadder
+{
+
+        public static void main(String[] args)
+        {
+                int start_pos=0;
+                System.out.println(start_pos);
+
+        }
+
+}
+
+
+
+
+
