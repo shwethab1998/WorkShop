07b6ab961d979a1375261c0224de4d43218201ed
“Shwetha <shwethabalaji87@gmail.com>
2020-11-27 14:07:10 +0530

    Player till 100

--- SnakesLadder.java
+++ SnakesLadder.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 public class SnakesLadder
 {
-        public int PLAYER_POS=0; //UC1, initializing player position as 0
-	public int die_roll,player=PLAYER_POS;
+        public static int PLAYER_POS=0; //UC1, initializing player position as 0
+	public static int die_roll,player=PLAYER_POS;
         public void rolldice()
         {
                 Random random=new Random();
@@ -11,40 +11,37 @@ public class SnakesLadder
 	public void Option()
 	{
 		while(player<=100)
-                {
-		rolldice();
+		{
 		Random option_random=new Random();
                 int option=option_random.nextInt(3);
 		switch(option)
 		{
                 case 1:
 			player+=die_roll;
-                        System.out.println("You got Ladder!!");
+                        System.out.println("You got Ladder!!:");
                 break;
 		case 0:
+			player=-die_roll;
 			if(player<0)
 			{
                         player=PLAYER_POS;
-                	}
-			else{
-			player=-die_roll;
-			}
-			System.out.println("You got Snake!!");
+	             	}
+			System.out.println("You got Snake!!: ");
 		break;
                 default:
                         player=player;
-			System.out.println("No Play!!");
+			System.out.println("No Play!!: ");
 		break;
-		}
-		}
-		System.out.println("Player Won, Position is now:"+player);
+		}}
         }
         public static void main(String[] args)
         {
                 SnakesLadder user=new SnakesLadder();
-                user.rolldice();   //UC2, dice roll b/w 1-6
+		user.rolldice();   //UC2, dice roll b/w 1-6
 		user.Option();     //UC3, dice output options
-        }
+		System.out.println("Position is:"+player);
+		System.out.println("Player won, Position is:"+player);
+	}
 }
 
 
