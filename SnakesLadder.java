de66d7668339d87082a64c01c2aa74cd95ae3b1c
“Shwetha <shwethabalaji87@gmail.com>
2020-11-27 14:28:42 +0530

    Exactly 100

--- SnakesLadder.java
+++ SnakesLadder.java
@@ -7,17 +7,20 @@ public class SnakesLadder
         {
                 Random random=new Random();
                 die_roll=random.nextInt(6)+1;
+		System.out.println(die_roll);
 	}
 	public void Option()
 	{
-		while(player<=100)
-		{
 		Random option_random=new Random();
                 int option=option_random.nextInt(3);
 		switch(option)
 		{
                 case 1:
 			player+=die_roll;
+			if(player>100)
+			{
+			player-=die_roll;
+			}
                         System.out.println("You got Ladder!!:");
                 break;
 		case 0:
@@ -29,17 +32,19 @@ public class SnakesLadder
 			System.out.println("You got Snake!!: ");
 		break;
                 default:
-                        player=player;
 			System.out.println("No Play!!: ");
 		break;
-		}}
+		}
         }
         public static void main(String[] args)
         {
                 SnakesLadder user=new SnakesLadder();
+		while(player<100)
+		{
 		user.rolldice();   //UC2, dice roll b/w 1-6
 		user.Option();     //UC3, dice output options
 		System.out.println("Position is:"+player);
+		}
 		System.out.println("Player won, Position is:"+player);
 	}
 }
